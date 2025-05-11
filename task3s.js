db71357778b4f24ed7b23d070da67f57d9730b0b
ArifMiah07 <arifmiah.me322@gmail.com>
2025-05-11 21:42:35 +0600

    task 3,4,5 done

--- task3s.js
+++ task3s.js
@@ -1,2 +1,18 @@
 // 3.Task: Function Composition
 // Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
+
+function composeOperations() {
+  const square = (num) => num * num;
+  const double = (num) => num * 2;
+  const addFive = (num) => num + 5;
+  
+  const composedFunction = (num) => addFive(double(square(num)));
+  
+  const input = 3;
+  const result = composedFunction(input);
+  
+  console.log(`Input: ${input}, Result after square, double, and add 5: ${result}`);
+  return result;
+}
+
+composeOperations();//3² = 9, 9*2 = 18, 18+5 = 23
