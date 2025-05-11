0a9721ddb23b89a55cf4feb27b8529c49b3030e3
ArifMiah07 <arifmiah.me322@gmail.com>
2025-05-11 19:01:42 +0600

    task 2 done

---
+++ task2s.js
@@ -0,0 +1,42 @@
+// 2.Task: Object Manipulation
+// Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
+
+const books = [
+    {
+        title: "Once upon a time",
+        author: "mirazde zuve",
+        year: 1011,
+    },
+    {
+        title: "Look there is a mocking bird!",
+        author: "David Naick",
+        year: 1911,
+    },
+    {
+        title: "Multiply by 3",
+        author: "Sir Neu jack",
+        year: 1611,
+    },
+    {
+        title: "La Mujher, el hombre",
+        author: "Cs Garcia",
+        year: 1991,
+    },
+    {
+        title: "Keu triple 9 e call deo",
+        author: "",
+        year: 1221,
+    },
+    {
+        title: "The most hated Summer",
+        author: "Smart Akbar",
+        year: 1614,
+    },
+]
+
+const onlyBooksTittle = (books)=> {
+    const titles = books.map(book=> book.title);
+    return titles;
+}
+const result = onlyBooksTittle(books);
+console.log(result);
